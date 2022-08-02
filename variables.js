11e696b94391954133d37c12cf5510225dfcf673
Courtney Lippman <courtney.lippman@colorado.edu>
2022-08-02 16:56:19 -0600

    Reassign original string values and add console.log statements

--- variables.js
+++ variables.js
@@ -1,9 +1,21 @@
-var email = "IAmAnEmail.gmail.com"
-var username = "Usernameishere"
+var email = "IAmAnEmail.gmail.com";
+var username = "Usernameishere";
 
-var age = 26
-var numberOfLikes = 300
 
-var likeTuring = true
-var liveInColorado = true
-var canSing = false
+email = "Iamanewemail";
+console.log(email);
+username = "Newusername";
+console.log(username);
+
+var age = 26;
+console.log(age);
+var numberOfLikes = 300;
+console.log(numberOfLikes);
+
+
+var likeTuring = true;
+console.log(likeTuring);
+var liveInColorado = true;
+console.log(liveInColorado);
+var canSing = false;
+console.log(canSing);
